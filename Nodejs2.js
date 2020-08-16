54383c1bea8e7ee44ecd05f2a18b39a411d2a864
Nabhay <67143992+Nabhay@users.noreply.github.com>
2020-08-16 21:42:00 +0530

    Create Nodejs2.js

---
+++ Nodejs2.js
@@ -0,0 +1,12 @@
+
+ 
+fetch('https://github.com/Nabhay')
+    .then(function(response) {
+        if (response.status >= 400) {
+            throw new Error("Bad response from server");
+        }
+        return response.json();
+    })
+    .then(function(stories) {
+        console.log(stories);
+    });
